1564cc8263991a421d69c31121bc9db408c97241
Magdy, Ismail (ext) (DI SW ICS MNA RD HAV1 A) <ismail.magdy.ext@siemens.com>
2024-05-07 03:58:25 +0300

    encapsulate logic inside Board class

--- GameLogic/alphaBetaPruning.py
+++ GameLogic/alphaBetaPruning.py
@@ -1,33 +1,35 @@
-import copy
 import math
+from board import Board
 
 
-def get_best_move(board, player_color, depth) -> (int, int):
-    moves = get_possible_moves(board, player_color)
+def get_best_move(board: Board, player_color, depth) -> (int, int):
+    moves = board.get_possible_cell_moves(player_color)
     max_score = -math.inf
-    best_move = [-1,-1]
+    best_move = [-1, -1]
 
     alpha = - math.inf
     beta = math.inf
     for _, move in enumerate(moves):
-        newBoard = update_board(board, move[0], move[1], player_color)
-        move_score = alpha_beta_pruning(newBoard, player_color, alpha, beta, depth, False)
+        clonedBoard = board.clone()
+        clonedBoard.add_piece(move[0], move[1], player_color)
+        move_score = alpha_beta_pruning(clonedBoard, player_color, alpha, beta, depth, False)
         if move_score > max_score:
             max_score = move_score
             best_move = move
     return best_move[0], best_move[1]
 
 
-def alpha_beta_pruning(board, player_color, alpha, beta, depth, is_maximizing) -> int:
-    moves = get_possible_moves(board, player_color)
+def alpha_beta_pruning(board: Board, player_color, alpha, beta, depth, is_maximizing) -> int:
+    moves = board.get_possible_cell_moves(player_color)
     if depth == 0 or len(moves) == 0:
         return evaluate(board, player_color)
 
     if is_maximizing:
         max_score = -math.inf
         for _, move in enumerate(moves):
-            newBoard = update_board(board, move[0], move[1], player_color)
-            move_score = alpha_beta_pruning(newBoard, 1 - player_color, alpha, beta, depth - 1, False)
+            clonedBoard = board.clone()
+            clonedBoard.add_piece(move[0], move[1], player_color)
+            move_score = alpha_beta_pruning(clonedBoard, 1 - player_color, alpha, beta, depth - 1, False)
             max_score = max(max_score, move_score)
             alpha = max(alpha, move_score)
             if beta <= alpha:
@@ -37,8 +39,9 @@ def alpha_beta_pruning(board, player_color, alpha, beta, depth, is_maximizing) -
     else:
         min_score = math.inf
         for _, move in enumerate(moves):
-            newBoard = update_board(board, move[0], move[1], player_color)
-            move_score = alpha_beta_pruning(newBoard, 1 - player_color, alpha, beta, depth - 1, True)
+            clonedBoard = board.clone()
+            clonedBoard.add_piece(move[0], move[1], player_color)
+            move_score = alpha_beta_pruning(clonedBoard, 1 - player_color, alpha, beta, depth - 1, True)
             min_score = min(min_score, move_score)
             beta = min(beta, move_score)
             if beta <= alpha:
@@ -46,89 +49,5 @@ def alpha_beta_pruning(board, player_color, alpha, beta, depth, is_maximizing) -
         return min_score
 
 
-def evaluate(board, player_color):
-    rows = len(board)
-    cols = len(board[0])
-    cnt = 0
-    for i in range(rows):
-        for j in range(cols):
-            cnt += (board[i][j] == player_color)
-    return cnt
-
-
-# tested
-# encapsulate
-def update_board(board, i, j, curr_player_color):
-    newBoard = copy.deepcopy(board)
-    flip_pieces(newBoard, i, j, curr_player_color, [0, 1])
-    flip_pieces(newBoard, i, j, curr_player_color, [0, -1])
-    flip_pieces(newBoard, i, j, curr_player_color, [-1, 0])
-    flip_pieces(newBoard, i, j, curr_player_color, [1, 0])
-    return newBoard
-
-
-# tested
-# encapsulate
-def get_possible_moves(board, curr_player_color):
-    valid_moves = []
-    rows = len(board)
-    cols = len(board[0])
-    for i in range(rows):
-        for j in range(cols):
-            score = calc_move_score(board, i, j, curr_player_color)
-            if score != 0:
-                valid_moves.append([i, j])
-    return valid_moves
-
-
-# tested
-def calc_move_score(board, i, j, curr_player_color):
-    right = get_num_of_possible_flips(board, i, j, curr_player_color, [0, 1])
-    left = get_num_of_possible_flips(board, i, j, curr_player_color, [0, -1])
-    up = get_num_of_possible_flips(board, i, j, curr_player_color, [-1, 0])
-    down = get_num_of_possible_flips(board, i, j, curr_player_color, [1, 0])
-    return right + left + up + down
-
-
-# tested
-# encapsulate
-def get_num_of_possible_flips(board, i, j, curr_player_color, change_of_coordinates):
-    if curr_player_color == -1 or board[i][j] != -1:
-        return 0
-    rows = len(board)
-    cols = len(board[0])
-    another_color_cnt = 0
-    another_player_color = 1 - curr_player_color
-    i += change_of_coordinates[0]
-    j += change_of_coordinates[1]
-    while rows > i >= 0 and cols > j >= 0:
-        if board[i][j] == -1:
-            return 0
-        if board[i][j] == curr_player_color:
-            return another_color_cnt
-        another_color_cnt += (board[i][j] == another_player_color)
-        i += change_of_coordinates[0]
-        j += change_of_coordinates[1]
-    return 0
-
-
-# tested
-# encapsulate
-def flip_pieces(board, i_play_position, j_play_position, curr_player_color, change_of_coordinates):
-    rows = len(board)
-    cols = len(board[0])
-    another_player_color = 1 - curr_player_color
-    count = get_num_of_possible_flips(board, i_play_position, j_play_position, curr_player_color, change_of_coordinates)
-    if not count:
-        return
-    i = i_play_position
-    j = j_play_position
-    while rows > i >= 0 and cols > j >= 0:
-        if board[i][j] == curr_player_color:
-            break
-        elif board[i][j] == another_player_color:
-            board[i][j] = curr_player_color
-        i += change_of_coordinates[0]
-        j += change_of_coordinates[1]
-    board[i_play_position][j_play_position] = curr_player_color
-
+def evaluate(board: Board, player_color):
+    return board.get_color_count(player_color)
--- GameLogic/board.py
+++ GameLogic/board.py
@@ -1,41 +1,80 @@
+import copy
+
+
 class Board:
-    def __init__(self, rows, cols):
-        self.rows = rows
-        self.cols = cols
-        self.board = []
-        for i in range(rows):
-            self.board[i] = []
-            for j in range(cols):
-                self.board[i][j] = -1
+    rows = 8
+    cols = 8
 
-    def add_piece(self, row, col, piece):
-        if 0 <= row < self.rows and 0 <= col < self.cols:
-            self.board[row][col] = piece
+    def __init__(self):
+        self.grid = [[-1 for _ in range(self.cols)] for _ in range(self.rows)]
+        self.grid[3][3] = 1
+        self.grid[3][4] = 0
+        self.grid[4][3] = 0
+        self.grid[4][4] = 1
 
-    def get_piece_at(self, row, col):
-        return self.board[row][col]
+    def get_color_count(self, color):
+        count = 0
+        for i in range(self.rows):
+            for j in range(self.cols):
+                count += (color == self.grid[i][j])
+        return count
 
-    def get_possible_moves(self, curr_player_color):
+    def add_piece(self, i, j, curr_player_color):
+        def flip_pieces(i_play_position, j_play_position, color, change_of_coordinates):
+            rows = len(self.grid)
+            cols = len(self.grid[0])
+            another_player_color = 1 - color
+            count = self._count_possible_flips_in_direction(i_play_position, j_play_position, curr_player_color, change_of_coordinates)
+            if not count:
+                return
+            temp_i = i_play_position
+            temp_j = j_play_position
+            while rows > temp_i >= 0 and cols > temp_j >= 0:
+                if self.grid[temp_i][temp_j] == curr_player_color:
+                    break
+                elif self.grid[temp_i][temp_j] == another_player_color:
+                    self.grid[temp_i][temp_j] = curr_player_color
+                temp_i += change_of_coordinates[0]
+                temp_j += change_of_coordinates[1]
+            self.grid[i_play_position][j_play_position] = curr_player_color
+
+        flip_pieces(i, j, curr_player_color, [0, 1])
+        flip_pieces(i, j, curr_player_color, [0, -1])
+        flip_pieces(i, j, curr_player_color, [-1, 0])
+        flip_pieces(i, j, curr_player_color, [1, 0])
+
+    def count_num_of_possible_flips(self, i_start, j_start, curr_player_color):
+        right = self._count_possible_flips_in_direction(i_start, j_start, curr_player_color, [0, 1])
+        left = self._count_possible_flips_in_direction(i_start, j_start, curr_player_color, [0, -1])
+        up = self._count_possible_flips_in_direction(i_start, j_start, curr_player_color, [-1, 0])
+        down = self._count_possible_flips_in_direction(i_start, j_start, curr_player_color, [1, 0])
+        return right + left + up + down
+
+    def _count_possible_flips_in_direction(self, i, j, curr_player_color, change_of_coordinates):
+        if curr_player_color == -1 or self.grid[i][j] != -1:
+            return 0
+        another_color_cnt = 0
+        another_player_color = 1 - curr_player_color
+        i += change_of_coordinates[0]
+        j += change_of_coordinates[1]
+        while self.rows > i >= 0 and self.cols > j >= 0:
+            if self.grid[i][j] == -1:
+                return 0
+            if self.grid[i][j] == curr_player_color:
+                return another_color_cnt
+            another_color_cnt += (self.grid[i][j] == another_player_color)
+            i += change_of_coordinates[0]
+            j += change_of_coordinates[1]
+        return 0
+
+    def get_possible_cell_moves(self, curr_player_color):
         valid_moves = []
         for i in range(self.rows):
             for j in range(self.cols):
-                valid_right = self.is_valid_cell(i, j, curr_player_color, [0, 1])
-                valid_left = self.is_valid_cell(i, j, curr_player_color, [0, -1])
-                valid_up = self.is_valid_cell(i, j, curr_player_color, [-1, 0])
-                valid_down = self.is_valid_cell(i, j, curr_player_color, [1, 0])
-                if valid_right  or valid_left or valid_up or valid_down:
+                score = self.count_num_of_possible_flips(i, j, curr_player_color)
+                if score != 0:
                     valid_moves.append([i, j])
+        return valid_moves
 
-    def get_num_of_flipped(self, row, col, curr_player_color, change_of_coordinates):
-        another_color_cnt = 0
-        another_player_color = 1 - curr_player_color
-        i = row
-        j = col
-        while self.rows > i >= 0 and self.cols > j >= 0:
-            if self.board[i][j] == curr_player_color:
-                break
-            i += change_of_coordinates[0]
-            j += change_of_coordinates[1]
-            another_color_cnt += (self.board[i][j] == another_player_color)
-
-        return another_color_cnt
+    def clone(self):
+        return copy.deepcopy(self)
